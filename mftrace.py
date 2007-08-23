2f7a9f2250627ab70eeb90862fd6c0538a11d2f6
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-08-22 22:43:17 -0300

    Remove spurious spaces in .enc filenames

--- mftrace.py
+++ mftrace.py
@@ -198,9 +198,9 @@ coding_dict = {
 
     # from TeTeX
     'TeX typewriter text': '09fbbfac.enc', # cmtt10
-    'TeX math symbols':'10037936.enc ', # cmbsy
+    'TeX math symbols':'10037936.enc', # cmbsy
     'ASCII caps and digits':'1b6d048e', # cminch
-    'TeX math italic': 'aae443f0.enc ', # cmmi10
+    'TeX math italic': 'aae443f0.enc', # cmmi10
     'TeX extended ASCII':'d9b29452.enc',
     'TeX text': 'f7b6d320.enc',
     'TeX text without f-ligatures': '0ef0afca.enc',
